804d4956ad853ff877a20b9814bfcb2f80a24ba5
whq <whq527@outlook.com>
2019-03-08 13:48:30 +0800

    更新

---
+++ Release/UpdateScripts.bat
@@ -0,0 +1,2 @@
+sqlite3.exe msgdb.db < UpdateScripts.sql
+pause
---
+++ Release/UpdateScripts.sql
@@ -0,0 +1,4 @@
+replace into data_relation values('data_file_check','ck.file.compare');
+replace into data_relation values('data_hq_check','ck.hq.sina');
+replace into data_relation values('data_hq_check','ck.hq.rmq');
+select * from data_relation;
